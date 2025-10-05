8aac5e11c70493165bb90ae90df974d26b740375
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-10-05 01:59:24 -0300

    feat: unlike video use-case

---
+++ src/use-cases/video/unlike-video.use-case.ts
@@ -0,0 +1,29 @@
+import { VideoRepository } from "../../domain/video/repository/video-repository";
+import { UseCase } from "../use-case";
+import { NotFoundException } from "../exceptions/not-found.exception";
+import { Video } from "../../domain/video/entities/video.entity";
+
+export type TUnlikeVideoUseCaseInputDto = {
+  id: string;
+};
+
+export type TUnlikeVideoUseCaseOutputDto = Video;
+
+export class UnlikeVideoUseCase extends UseCase<
+  TUnlikeVideoUseCaseInputDto,
+  TUnlikeVideoUseCaseOutputDto
+> {
+  constructor(private readonly repository: VideoRepository) {
+    super();
+  }
+
+  async execute(
+    input: TUnlikeVideoUseCaseInputDto,
+  ): Promise<TUnlikeVideoUseCaseOutputDto> {
+    const video = await this.repository.findById(input.id);
+    if (!video) throw new NotFoundException("Video not found.");
+    video.decreaseLikeCount();
+    await this.repository.save(video);
+    return video;
+  }
+}
