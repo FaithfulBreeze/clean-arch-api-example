19f08784f4639f5b0e09e788fb79d30aa78d955c
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-10-05 22:37:04 -0300

    feat: find video by id controller

---
+++ src/presentation/http/video/find-video-by-id.controller.ts
@@ -0,0 +1,29 @@
+import { VideoProps } from "src/domain/video/entities/video.entity";
+import {
+  TFindVideoByIdUseCaseInputDto,
+  TFindVideoByIdUseCaseOutputDto,
+} from "../../../use-cases/video/find-video-by-id.use-case";
+import { Controller, ControllerProps } from "../controller";
+
+export class FindVideoByIdController extends Controller<
+  TFindVideoByIdUseCaseInputDto,
+  TFindVideoByIdUseCaseOutputDto,
+  VideoProps
+> {
+  constructor(
+    props: ControllerProps<
+      TFindVideoByIdUseCaseInputDto,
+      TFindVideoByIdUseCaseOutputDto
+    >,
+  ) {
+    super(props);
+  }
+
+  async handle(
+    inputDto: TFindVideoByIdUseCaseInputDto,
+    params: { id: string },
+  ): Promise<{ response: VideoProps; status: number }> {
+    const video = await this.props.handler.execute({ id: params.id });
+    return { response: video.props, status: 200 };
+  }
+}
